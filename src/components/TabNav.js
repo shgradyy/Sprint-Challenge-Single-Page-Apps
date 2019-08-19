58b9651a359d5081903f1584cc644cb7afa49dc2
Shantay Grady <shantaygrady@gmail.com>
2019-08-19 19:58:31 -0400

    Sets up TabNav and restarts dependencies

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -4,5 +4,9 @@ import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
+    const [tab, setTab] = useState([]);
 
+    const handleChange = event => {
+        setUser({ ...user, [event.target.name]: event.target.value });
+      };
 };
