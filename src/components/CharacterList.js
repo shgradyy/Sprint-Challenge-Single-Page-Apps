a105ec340ba18399cd7e583068337ad6e44cbcbb
Shantay Grady <shantaygrady@gmail.com>
2019-08-19 22:32:36 -0400

    Creates CharacterList API request

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+const [character, setCharacter] = useState({name: ""});
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(result => {
+      setCharacter(result.data.results);
+    })
+    .catch(error => {
+      console.log("There's an error", error);
+    })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
